de867814b1041c3ad17c2b924a48030696ccf4d2
Vinay Kumar Solanki <148608401+Vinay-100lanki@users.noreply.github.com>
2024-04-18 14:56:54 +0530

    first commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,48 +2,54 @@ import { useState, useEffect } from "react";
 import Spinner from "../components/Spinner";
 import Product from "../components/Product";
 
-const Home = () => {
-  // const API_URL = "http://20.244.56.144/test/register";
-  const API_URL = "https://fakestoreapi.com/products";
+const Home = () => { 
+
+  const API_URL = "http://20.244.56.144/test";
+  const AUTH_TOKEN = "dKAMsn";
+
   const [loading, setLoading] = useState(false);
   const [posts, setPosts] = useState([]);
 
   async function fetchProductData() {
     setLoading(true);
 
-    try{
-      const res = await fetch(API_URL);
+    try {
+      const res = await fetch(API_URL, {
+        method: 'POST',
+        headers: {
+          'Authorization': AUTH_TOKEN, 
+          'Content-Type': 'application/json'
+        }, 
+      });
       const data = await res.json();
 
       setPosts(data);
-    }
-    catch(error) {
-      console.log("Error aagya ji");
+    } catch (error) {
+      console.log("Error:", error);
       setPosts([]);
     }
     setLoading(false);
   }
 
-  useEffect( () => {
+  useEffect(() => {
     fetchProductData();
-  },[])
+  }, []);
 
   return (
     <div>
-      {
-        loading ? <Spinner />  :
-        posts.length > 0 ? 
-        (<div className="grid  xs:gridcols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
-          {
-            posts.map( (post) => (
-            <Product key = {post.id} post={post}/>
-          ) )
-          }
-        </div>) :
+      {loading ? (
+        <Spinner />
+      ) : posts.length > 0 ? (
+        <div className="grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 max-w-6xl p-2 mx-auto space-y-10 space-x-5 min-h-[80vh]">
+          {posts.map((post) => (
+            <Product key={post.id} post={post} /> 
+          ))}
+        </div>
+      ) : (
         <div className="flex justify-center items-center">
           <p>No Data Found</p>
-        </div> 
-      }
+        </div>
+      )}
     </div>
   );
 };
