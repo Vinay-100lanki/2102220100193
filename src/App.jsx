a6948ffcb030b9731cd2a4856df69997a96238cc
Vinay Kumar Solanki <148608401+Vinay-100lanki@users.noreply.github.com>
2024-04-18 15:07:51 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { Routes } from "react-router-dom";
 import Navbar from "./components/Navbar"
 import { Route } from "react-router-dom";
 import Home from "./pages/Home"
-import Cart from "./pages/Cart"
 
 const App = () => {
   return (<div>
@@ -11,7 +10,6 @@ const App = () => {
         </div>
         <Routes>
           <Route path="/" element={<Home/>} />
-          <Route path="/cart" element={<Cart/>} />
         </Routes>
   </div>)
 };
